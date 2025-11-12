62fd9b22eb8e5335cf8e75862728d0ea74dfbe52
Hemant <guptahemant350@gmail.com>
2025-11-12 21:06:32 +0530

    Added webhook trigger testing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins Multi-Stage Pipeline!");
         System.out.println("This is a simple Java application.Added a webhook trigger.");
+        System.out.println("This line is added to test the webhook.");
     }
 }
